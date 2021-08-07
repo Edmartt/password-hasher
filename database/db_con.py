9dbc04e935f6d29db1fbf5e60a2c1c881e338a01
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-07 18:47:17 -0500

    Now the get connection method is inside a class

--- database/db_con.py
+++ database/db_con.py
@@ -3,19 +3,27 @@ from os import environ
 import mysql.connector
 
 
-def get_connection():
-    """Connect to the db server.
+class Database:
 
-    For a succesfull connection, you to set
-    the environment variables DB_HOST, DB_USER,
-    DB_PASSWORD and DB_NAME
-    """
-    connection = mysql.connector.connect(
-        host=environ.get("DB_HOST"),
-        user=environ.get("DB_USER"),
-        password=environ.get("DB_PASSWORD"),
-        database=environ.get("DB_NAME")
-        )
-    cursor = connection.cursor(dictionary=True)
+    host = environ.get("DB_HOST")
+    user = environ.get("DB_USER")
+    password = environ.get("DB_PASSWORD")
+    database = environ.get("DB_NAME")
 
-    return connection, cursor
+    def get_connection(self):
+        """Connect to the db server.
+
+        For a succesfull connection, you to set
+        the environment variables DB_HOST, DB_USER,
+        DB_PASSWORD and DB_NAME
+        """
+        connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+            )
+
+        cursor = connection.cursor(dictionary=True)
+
+        return connection, cursor
