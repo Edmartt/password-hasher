6cb44499106803ddf1c4f7fd8093d9ccc7f56369
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-11 01:03:48 -0500

    Fixes the useless class attributes. Now using instance attributes instead and the database server connector is now mariadb

--- database/db_con.py
+++ database/db_con.py
@@ -1,33 +1,35 @@
 """Connection for MySQL/MariaDB server."""
 import logging
 from os import environ
-import mysql.connector
+import mariadb
+from .iconnector import IDatabaseConnector
 
 
-class Database:
+class MariaDatabase(IDatabaseConnector):
 
-    host = environ.get("DB_HOST")
-    user = environ.get("DB_USER")
-    password = environ.get("DB_PASSWORD")
-    database = environ.get("DB_NAME")
+    def __init__(self):
+        self.host = environ.get("DB_HOST")
+        self.user = environ.get("DB_USER")
+        self.password = environ.get("DB_PASSWORD")
+        self.database = environ.get("DB_NAME")
 
     def get_connection(self):
         """Connect to the db server.
 
-        For a succesfull connection, you to set
+        For a succesfull connection, you need to set
         the environment variables DB_HOST, DB_USER,
         DB_PASSWORD and DB_NAME
         """
         try:
-            connection = mysql.connector.connect(
-                host=self.host,
-                user=self.user,
-                password=self.password,
-                database=self.database
-                )
+            connection = mariadb.connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database
+                    )
 
             cursor = connection.cursor(dictionary=True)
             return connection, cursor
 
-        except Exception as ex:
-            logging.exception('Error detected')
+        except mariadb.ProgrammingError as ex:
+            logging.exception(ex)
